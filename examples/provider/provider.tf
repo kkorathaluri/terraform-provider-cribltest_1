64ed0d2855c7870db685fe5eb04b7d5127cd479b
Kishore Korathaluri <kkorathaluri@cribl.io>
2025-03-31 11:02:55 +1100

    adding new repo.

---
+++ examples/provider/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    cribl-terraform = {
+      source  = "speakeasy/cribl-terraform"
+      version = "0.0.2"
+    }
+  }
+}
+
+provider "cribl-terraform" {
+  # Configuration options
+}
